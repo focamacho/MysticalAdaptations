5d45f4b69df1369a47445b1e72798bede469ecd8
Renan Colombo <48870573+Focamacho@users.noreply.github.com>
2019-06-22 15:28:39 -0300

    Fix Wrong Seed Extractor Attack Speed

--- src/main/java/com/focamacho/mysticaladaptations/items/SeedExtractor.java
+++ src/main/java/com/focamacho/mysticaladaptations/items/SeedExtractor.java
@@ -356,7 +356,7 @@ public class SeedExtractor extends Item implements IHasModel{
 
         if(equipmentSlot == EntityEquipmentSlot.MAINHAND){
             multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.toolMaterial.getAttackDamage() - 1.0F, 0));
-            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.5D, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -3.4D, 0));
         }
         return multimap;
     }
