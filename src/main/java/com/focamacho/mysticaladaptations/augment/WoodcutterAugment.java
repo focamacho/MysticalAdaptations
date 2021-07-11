5dc370589fe736e3b4657b2e985303e8e0b261de
Focamacho <renancolombomail@gmail.com>
2021-07-11 13:24:06 -0300

    Simplify some code

--- src/main/java/com/focamacho/mysticaladaptations/augment/WoodcutterAugment.java
+++ src/main/java/com/focamacho/mysticaladaptations/augment/WoodcutterAugment.java
@@ -11,14 +11,18 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.Objects;
 
+/*
+ * TO-DO:
+ * Make it cuts the entire tree
+ */
 public class WoodcutterAugment extends Augment {
 
     public WoodcutterAugment(ResourceLocation id) {
@@ -28,20 +32,14 @@ public class WoodcutterAugment extends Augment {
     @Override
     public boolean onBlockStartBreak(ItemStack stack, BlockPos pos, PlayerEntity player) {
         World world = player.getCommandSenderWorld();
-        BlockRayTraceResult trace = BlockHelper.rayTraceBlocks(world, player);
-        int side = trace.getDirection().ordinal();
-        return !this.harvest(stack, world, pos, side, player);
+        return !this.harvest(stack, world, pos, player);
     }
 
-    private boolean harvest(ItemStack stack, World world, BlockPos pos, int side, PlayerEntity player) {
-        boolean woodcutter = true;
-
-        if (player.isCrouching()) {
-            woodcutter = false;
-        }
+    private boolean harvest(ItemStack stack, World world, BlockPos pos, PlayerEntity player) {
+        boolean woodcutter = !player.isCrouching();
 
         BlockPos blockPos = pos;
-        List<BlockPos> toHarvest = new ArrayList<BlockPos>();
+        List<BlockPos> toHarvest = new ArrayList<>();
 
         if(woodcutter) {
             do {
@@ -56,9 +54,7 @@ public class WoodcutterAugment extends Augment {
             return false;
         } else {
             Block block = state.getBlock();
-            if (woodcutter && hardness >= 0.2F && block.getHarvestTool(state) == null || stack.getToolTypes().stream().anyMatch((t) -> {
-                return block.isToolEffective(state, t);
-            })) {
+            if (woodcutter && hardness >= 0.2F && block.getHarvestTool(state) == null || stack.getToolTypes().stream().anyMatch((t) -> block.isToolEffective(state, t))) {
                 toHarvest.forEach((woodPos) -> {
                     if(woodPos != pos){
                         this.tryHarvest(world, woodPos, true, stack, player);
@@ -75,10 +71,11 @@ public class WoodcutterAugment extends Augment {
         float hardness = state.getDestroySpeed(world, pos);
         Item item = stack.getItem();
         boolean harvest = (ForgeHooks.canHarvestBlock(state, player, world, pos) || item.canHarvestBlock(stack, state)) && (!extra || item.getDestroySpeed(stack, world.getBlockState(pos)) > 1.0F);
-        return hardness < 0.0F || extra && !harvest ? false : BlockHelper.breakBlocksAOE(stack, world, player, pos);
+        return !(hardness < 0.0F) && (!extra || harvest) && BlockHelper.breakBlocksAOE(stack, world, player, pos);
     }
 
     private boolean isWood(Block block) {
-        return BlockTags.getAllTags().getTag(new ResourceLocation("minecraft:logs")).contains(block);
+        //Tag minecraft:logs never will be null
+        return Objects.requireNonNull(BlockTags.getAllTags().getTag(new ResourceLocation("minecraft", "logs"))).contains(block);
     }
 }
