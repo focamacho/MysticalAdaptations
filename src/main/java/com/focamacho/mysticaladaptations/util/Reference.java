07c86aade5c333bc69940ff189efd55b7efdc36b
Focamacho <renancolombomail@gmail.com>
2021-12-21 04:49:54 -0300

    Version++

--- src/main/java/com/focamacho/mysticaladaptations/util/Reference.java
+++ src/main/java/com/focamacho/mysticaladaptations/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "mysticaladaptations";
 	public static final String NAME = "Mystical Adaptations";
-	public static final String VERSION = "1.8.5";
+	public static final String VERSION = "1.8.6";
 	public static final String DEPENDENCIES = "required-after:mysticalagriculture@[1.7.5,);after:mysticalagradditions@[1.3.2,)";
 	public static final String CLIENT_PROXY_CLASS = "com.focamacho.mysticaladaptations.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.focamacho.mysticaladaptations.proxy.CommonProxy";
