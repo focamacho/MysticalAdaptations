1817685f9a87be4ec320a3dcf4998b4eb22d6c1c
focamacho <renancolombomail@gmail.com>
2023-02-12 19:25:34 -0300

    refactor: remove deprecated vampirism method

--- src/main/java/com/focamacho/mysticaladaptations/compat/vampirism/CompatVampirism.java
+++ src/main/java/com/focamacho/mysticaladaptations/compat/vampirism/CompatVampirism.java
@@ -5,19 +5,19 @@ import de.teamlapen.vampirism.entity.player.vampire.VampirePlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
 
 public class CompatVampirism {
 
     public static void fillThirst(Player player, Level world) {
         if(!world.isClientSide) {
-            VampirePlayer vampire = VampirePlayer.get(player);
-            vampire.drinkBlood(vampire.getBloodStats().getMaxBlood(), 0, false);
+            LazyOptional<VampirePlayer> optVampire = VampirePlayer.getOpt(player);
+            optVampire.ifPresent(vampire -> vampire.drinkBlood(vampire.getBloodStats().getMaxBlood(), 0, false));
         }
     }
 
     public static void applySunscreen(Player player, Level world) {
         MobEffectInstance effect = new MobEffectInstance(ModEffects.SUNSCREEN.get(), 20, 5, false, false);
-        if(world.isClientSide) effect.setNoCounter(true);
         player.addEffect(effect);
     }
 
