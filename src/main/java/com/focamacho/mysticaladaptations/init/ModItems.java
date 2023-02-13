d617ca064fff5de51e0d2f896dc506c3b059f429
focamacho <renancolombomail@gmail.com>
2023-02-13 02:57:17 -0300

    refactor: suppress unused warnings

--- src/main/java/com/focamacho/mysticaladaptations/init/ModItems.java
+++ src/main/java/com/focamacho/mysticaladaptations/init/ModItems.java
@@ -18,6 +18,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+@SuppressWarnings("unused")
 public class ModItems {
 
 	public static final DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MOD_ID);
