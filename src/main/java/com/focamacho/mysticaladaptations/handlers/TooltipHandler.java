157baa60a141da5029f7c46e12f61b07275cfcef
focamacho <renancolombomail@gmail.com>
2023-02-13 02:38:51 -0300

    fix: tier 6 tooltip replace

--- src/main/java/com/focamacho/mysticaladaptations/handlers/TooltipHandler.java
+++ src/main/java/com/focamacho/mysticaladaptations/handlers/TooltipHandler.java
@@ -2,19 +2,27 @@ package com.focamacho.mysticaladaptations.handlers;
 
 import com.blakebr0.mysticalagriculture.MysticalAgriculture;
 import com.blakebr0.mysticalagriculture.lib.ModTooltips;
-import com.focamacho.mysticaladaptations.MysticalAdaptations;
+import com.focamacho.mysticaladaptations.util.Reference;
 import com.focamacho.mysticaladaptations.util.Utils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class TooltipHandler {
 
     //Replace Tier 6 tooltips with a colored one
     @SubscribeEvent
     public void tooltipEvent(ItemTooltipEvent event){
-        if(event.getItemStack().getItem().getItemCategory() == MysticalAdaptations.creativeTab || event.getItemStack().getItem().getItemCategory() == MysticalAgriculture.CREATIVE_TAB) {
+        ResourceLocation rl = ForgeRegistries.ITEMS.getKey(event.getItemStack().getItem());
+        if(rl == null) return;
+        String modid = rl.getNamespace();
+
+        System.out.println("modid = " + modid);
+
+        if(modid.equals(Reference.MOD_ID) || modid.equals(MysticalAgriculture.MOD_ID)) {
             int index = -1;
             for(Component text : event.getToolTip()){
                 if(text.equals(ModTooltips.getTooltipForTier(6))){
